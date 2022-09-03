eab03145fa2d04c8e550cbe754f5c193fdd957c6
Philip O'Toole <philip.otoole@yahoo.com>
2022-09-03 17:37:44 -0400

    Flag if RANDOM() actually found

--- rewriter.go
+++ rewriter.go
@@ -1,15 +1,21 @@
 package sql
 
+import (
+	"strings"
+)
+
 type Rewriter struct {
 	RewriteRand bool
+
+	randRewritten bool
 }
 
-func (rw *Rewriter) Do(stmt Statement) (Statement, error) {
+func (rw *Rewriter) Do(stmt Statement) (Statement, bool, error) {
 	err := Walk(rw, stmt)
 	if err != nil {
-		return nil, err
+		return nil, false, err
 	}
-	return stmt, nil
+	return stmt, rw.randRewritten, nil
 }
 
 func (rw *Rewriter) Visit(node Node) (w Visitor, err error) {
@@ -23,6 +29,8 @@ func (rw *Rewriter) Visit(node Node) (w Visitor, err error) {
 		// Don't rewrite any further down this branch
 		return nil, nil
 	case *Call:
+		rw.randRewritten =
+			rw.randRewritten || strings.ToUpper(n.Name.Name) == "RANDOM"
 		n.Eval = rw.RewriteRand
 		return rw, nil
 	}
