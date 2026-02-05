2b6cd9e6b8c843f4e1cfe786bcb722326858fe03
Philip O'Toole <philip.otoole@yahoo.com>
2026-02-05 09:31:00 -0500

    Revert

--- parser_test.go
+++ parser_test.go
@@ -4017,6 +4017,33 @@ func TestParser_ParseStatement(t *testing.T) {
 			}},
 		})
 
+		// Test table alias with AS keyword
+		AssertParseStatement(t, `UPDATE vals AS v SET a=upper(v.a)`, &sql.UpdateStatement{
+			Update: pos(0),
+			Table: &sql.QualifiedTableName{
+				Name:  &sql.Ident{NamePos: pos(7), Name: "vals"},
+				As:    pos(12),
+				Alias: &sql.Ident{NamePos: pos(15), Name: "v"},
+			},
+			Set: pos(17),
+			Assignments: []*sql.Assignment{{
+				Columns: []*sql.Ident{{NamePos: pos(21), Name: "a"}},
+				Eq:      pos(22),
+				Expr: &sql.Call{
+					Name:   &sql.Ident{NamePos: pos(23), Name: "upper"},
+					Lparen: pos(28),
+					Args: []sql.Expr{
+						&sql.QualifiedRef{
+							Table:  &sql.Ident{NamePos: pos(29), Name: "v"},
+							Dot:    pos(30),
+							Column: &sql.Ident{NamePos: pos(31), Name: "a"},
+						},
+					},
+					Rparen: pos(32),
+				},
+			}},
+		})
+
 		// Test schema.table.column in expressions (issue fix)
 		AssertParseStatement(t, `UPDATE main.vals SET a=lower(main.vals.a)`, &sql.UpdateStatement{
 			Update: pos(0),
